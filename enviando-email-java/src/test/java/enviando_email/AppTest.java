165724424ffb9157c5a11a647aa0dafe23e52bd0
Murilo517 <murilosena07@hotmail.com>
2024-01-17 19:10:58 -0300

    teste

--- enviando-email-java/src/test/java/enviando_email/AppTest.java
+++ enviando-email-java/src/test/java/enviando_email/AppTest.java
@@ -20,34 +20,7 @@ public class AppTest {
 	public void testeEmail() {
 
 		try {
-			Properties properties = new Properties();
 			
-			properties.put("mail.smtp.ssl.trust", "*");
-			properties.put("mail.smtp.auth", "true");
-			properties.put("mail.smtp.starttls", "true");
-			properties.put("mail.smtp.host", "smtp.gmail.com");
-			properties.put("mail.smtp.port", "465");
-			properties.put("mail.smtp.socketFactory.port", "465");
-			properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-
-			Session session = Session.getInstance(properties, new Authenticator() {
-
-				@Override
-				protected PasswordAuthentication getPasswordAuthentication() {
-
-					return new PasswordAuthentication(userName, password);
-				}
-			});
-
-			Address[] toUser = InternetAddress.parse("murilosena07@hotmail.com,enviaremailjavamurilo517@gmail.com");
-
-			Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(userName));
-			message.setRecipients(Message.RecipientType.TO, toUser);
-			message.setSubject("Chegou esse email enviado com java");
-			message.setText("Olha que legal, parecer que deu certo estudar java");
-
-			Transport.send(message);
 
 		} catch (Exception e) {
 			e.printStackTrace();
